731ce509a3bf5f0d91c15aef3db3e00d2bfe789a
EnzoFerroni <enzoferroni@hotmail.com>
2024-05-20 00:33:09 -0300

    base

--- PV/urls.py
+++ PV/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from PV.views import index
+from PV.views import index, sobre, cadastro
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('sobre/', sobre, name='sobre'),
+    path('cadastro/', cadastro, name='cadastro'),
 ]
--- PV/views.py
+++ PV/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'clinica/index.html')
+
+def sobre(request):
+    return render(request, 'clinica/sobre.html')
+
+def cadastro(request):
+    return render(request, 'clinica/cadastro.html')
